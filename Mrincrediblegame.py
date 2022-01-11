8b89d9896ee4cedbe34956871c2e4a6163ecaaf6
Ahmet Alper Yanık <a.a.y103246@gmail.com>
2022-01-12 02:19:48 +0300

    ağaçlar ve bulutlar

--- Mrincrediblegame.py
+++ Mrincrediblegame.py
@@ -1,5 +1,7 @@
-import pygame, sys, os, random
-
+import pygame, sys, os, random, noise
+import math 
+from random import randrange
+import pygame.sprite as sprite
 clock = pygame.time.Clock()
 
 from pygame.locals import *
@@ -7,13 +9,16 @@ pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init() # initiates pygame
 pygame.mixer.set_num_channels(64)
 
+SCREEN_WIDTH = 1900
+SCREEN_HEIGHT = 1060
+
 pygame.display.set_caption('Pygame Mrincredible')
 
-WINDOW_SIZE = (1900,1060)
+WINDOW_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
   
-screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate the window
+screen = pygame.display.set_mode(WINDOW_SIZE) # initiate the window
 
-display = pygame.Surface((300,200)) # used as the surface for rendering, which is scaled
+display = pygame.Surface((300,200))# used as the surface for rendering, which is scaled
 
 moving_right = False
 moving_left = False
@@ -31,17 +36,26 @@ def generate_chunk(x,y):
             target_x = x * CHUNK_SIZE + x_pos
             target_y = y * CHUNK_SIZE + y_pos
             tile_type = 0 # nothing
-            if target_y > 10:
+            height = int(noise.pnoise1(target_x * 0.1, repeat=9999999) * 5)
+            if target_y > 8 - height:
                 tile_type = 2 # dirt
-            elif target_y == 10:
+            elif target_y == 8 - height:
                 tile_type = 1 # grass
-            elif target_y == 9:
-                if random.randint(1,5) == 1:
+            elif target_y == 8 - height - 1:
+                if random.randint(1,5) == 1:       
                     tile_type = 3 # plant
+            elif target_y == 1 - height - 1:        
+                if random.randint(1,16) == 1:
+                    tile_type = 4
+            elif target_y == 3 - height - 1:
+                if random.randint(1,16) == 1:
+                    tile_type = 5  
+            elif target_y == 0 - height - 1:
+                if random.randint(1,10) == 1:            
+                    tile_type = 6              
             if tile_type != 0:
                 chunk_data.append([[target_x,target_y],tile_type])
     return chunk_data
-
 
 global animation_frames
 animation_frames = {}
@@ -68,8 +82,7 @@ def change_action(action_var,frame,new_value):
         action_var = new_value
         frame = 0
     return action_var,frame
-        
-
+ 
 animation_database = {}
 
 animation_database['run'] = load_animation('player_animations/run',[7,7])
@@ -80,13 +93,19 @@ game_map = {}
 
 grass_img = pygame.image.load('grass.png')
 dirt_img = pygame.image.load('dirt.png')
+largetree_img = pygame.image.load('Largetree1.png')
+smalltree_img = pygame.image.load('Smalltree1.png')
+cloud_img = pygame.image.load('cloud.png')
 plant_img = pygame.image.load('plant.png').convert()
 plant_img.set_colorkey((255,255,255))
 
 tile_index = {1:grass_img,
-              2:dirt_img,
-              3:plant_img
-              }
+            2:dirt_img,
+            3:plant_img,
+            4:largetree_img,
+            5:smalltree_img,
+            6:cloud_img
+            }
 
 jump_sound = pygame.mixer.Sound('jump.wav')
 grass_sounds = [pygame.mixer.Sound('grass_0.wav'),pygame.mixer.Sound('grass_1.wav')]
@@ -106,8 +125,6 @@ player_flip = False
 grass_sound_timer = 0
 
 player_rect = pygame.Rect(100,100,5,22)
-
-background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
 
 def collision_test(rect,tiles):
     hit_list = []
@@ -139,24 +156,17 @@ def move(rect,movement,tiles):
     return rect, collision_types
 
 while True: # game loop
-    display.fill((146,244,255)) # clear screen by filling it with blue
-
+        
+    display.fill((135,206,235))# clear screen by filling it with sky blue
+    
     if grass_sound_timer > 0:
-        grass_sound_timer -= 1
+           grass_sound_timer -= 1    
 
     true_scroll[0] += (player_rect.x-true_scroll[0]-152)/20
     true_scroll[1] += (player_rect.y-true_scroll[1]-106)/20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
-
-    pygame.draw.rect(display,(7,80,75),pygame.Rect(0,120,300,80))
-    for background_object in background_objects:
-        obj_rect = pygame.Rect(background_object[1][0]-scroll[0]*background_object[0],background_object[1][1]-scroll[1]*background_object[0],background_object[1][2],background_object[1][3])
-        if background_object[0] == 0.5:
-            pygame.draw.rect(display,(20,170,150),obj_rect)
-        else:
-            pygame.draw.rect(display,(15,76,73),obj_rect)
 
     tile_rects = []
     for y in range(3):
@@ -226,10 +236,12 @@ while True: # game loop
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
-            
-
+         
     screen.blit(pygame.transform.scale(display,WINDOW_SIZE),(0,0))
     pygame.display.update()
     clock.tick(60)
     
-   
+    
+
+    
+
